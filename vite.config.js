e92a5b971380b56ac69202c5a0cd858d4c7e8c5b
rfkeepers <pinderlogic@gmail.com>
2021-08-29 13:14:57 -0600

    source to subfolder

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-const srcPath = path.resolve(__dirname, './src');
+const srcPath = path.resolve(__dirname, './deathless/src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
