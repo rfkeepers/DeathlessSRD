28af9dc372c718494eadcd8e211b098167fa7c9a
Ryan Keepers <pinderlogic@gmail.com>
2022-02-03 06:45:46 -0700

    vueconfig only, no slash

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-    publicPath: '',
-};
 // module.exports = {
-//     publicPath: process.env.NODE_ENV === 'production'? '/deathless' : ''
+//     publicPath: '',
 // };
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'? '/deathless' : ''
+};
 // "build": "vite build --base=/deathless/",
 // "baseUrl": "./deathless",
