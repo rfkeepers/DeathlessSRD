74f0920e56289c0831f69f88a5a2de5d39669252
Ryan Keepers <pinderlogic@gmail.com>
2022-02-03 06:57:57 -0700

    vue pkg, slash

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@
 //     publicPath: '',
 // };
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'? '/deathless/' : '/'
+    publicPath: process.env.NODE_ENV === 'production'? '/deathless' : ''
 };
 // "build": "vite build --base=/deathless/",
